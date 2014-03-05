f8e903653e11fa6b8039d2f90f657c57d3c54761
Brian Coleman <brian@meloyelo.com>
2014-03-05 13:38:00 -0700

    Installed Nunit from NuGet Created the CalculatorTests class and added the first 2 basic tests

---
+++ UI.Console/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UIConsole
+{
+	[TestFixture]
+	public class CalculatorTests
+	{
+		[Test]
+		public void TestValidValuesGetsCorrectSum()
+		{
+			var calculator = new Calculator();
+			Assert.AreEqual(calculator.Sum("1", "1"), 2);
+		}
+
+		[Test]
+		public void TestInvalidValuesThrows()
+		{
+			var calculator = new Calculator();
+			Assert.Throws<ArgumentException>(() => calculator.Sum("a", "1"));
+		}
+	}
+}

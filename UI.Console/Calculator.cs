9d936bc501590915aeab969c9a3a8a2eea2982a9
Brian Coleman <brian@meloyelo.com>
2014-03-05 12:39:05 -0700

    The first commit with the step 0

---
+++ UI.Console/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace UIConsole
+{
+	public class Calculator
+	{
+		public float Sum(string value1, string value2)
+		{
+			float fval1, fval2;
+			bool valid = true;
+
+			if (!float.TryParse(value1, out fval1))
+			{
+				valid = false;
+				WriteToLog("Not a valid number: " + value1);
+			}
+
+			if (!float.TryParse(value2, out fval2))
+			{
+				valid = false;
+				WriteToLog("Not a valid number: " + value2);
+			}
+
+			if (!valid)
+			{
+				throw new ArgumentException("One of the values is not valid");
+			}
+			return fval1 + fval2;
+		}
+
+		private void WriteToLog(string text)
+		{
+			if (text != null)
+			{
+				var fi = new FileInfo(@"C:\Temp\CalculatorLog\calc-log-" + DateTime.Now.ToString("yyyyMMdd") + ".log");
+				if (!fi.Exists)
+				{
+					using (var file = fi.CreateText())
+					{
+						file.WriteLine(text);
+					}
+				}
+				else
+				{
+					using (var file = fi.AppendText())
+					{
+						file.WriteLine(text);
+					}
+				}
+			}
+
+		}
+	}
+}
---
+++ UI.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UIConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var calculator = new Calculator();
+			Console.WriteLine("Calculating the sum of two values");
+			Console.WriteLine("=================================");
+			Console.Write("enter value 1: ");
+			string value1 = Console.ReadLine();
+			Console.Write("enter value 2: ");
+			string value2 = Console.ReadLine();
+
+			while (!string.Equals(value1, "q", StringComparison.InvariantCultureIgnoreCase))
+			{
+				try
+				{
+					Console.WriteLine("{0} + {1} = {2}", value1, value2, calculator.Sum(value1, value2));
+				}
+				catch (ArgumentException argumentException)
+				{
+					Console.WriteLine("At least one of the values is not a valid number");
+				}
+				Console.Write("\nenter value 1: ");
+				value1 = Console.ReadLine();
+				Console.Write("enter value 2: ");
+				value2 = Console.ReadLine();
+			}
+
+		}
+	}
+}
